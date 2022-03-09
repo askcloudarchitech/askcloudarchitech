eb42111fda25587d4762f7bbdbd612bbdcd016b6
Gary Morse <gmorse81@gmail.com>
2022-03-09 10:10:19 -0500

    debug environment

--- asksitestatic/netlify/functions/deploy-succeeded/main.go
+++ asksitestatic/netlify/functions/deploy-succeeded/main.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"os"
 
 	"github.com/askcloudarchitech/mediumautopost/pkg/mediumautopost"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type RequestBody struct {
+	Payload Payload `json:"payload"`
+}
+
+type Payload struct {
+	Context string `json:"context"`
+}
+
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 
-	fmt.Println(request)
+	requestBody := RequestBody{}
+	json.Unmarshal([]byte(request.Body), &requestBody)
 
-	if os.Getenv("CONTEXT") == "production" {
+	if requestBody.Payload.Context == "production" {
 		mediumautopost.Do("")
 	} else {
-		fmt.Println("context " + os.Getenv("CONTEXT") + " detected, skipping")
+		fmt.Println("context " + requestBody.Payload.Context + " detected, skipping")
 	}
 
 	return &events.APIGatewayProxyResponse{
