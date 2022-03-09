266ee901ed909eb915a9ea1fee24c9bd0d890e2d
Gary Morse <gmorse81@gmail.com>
2022-03-09 00:46:08 -0500

    try function

---
+++ netlify/functions/deploy-succeeded/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+
+	fmt.Println(ctx)
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode:        200,
+		Headers:           map[string]string{"Content-Type": "text/plain"},
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body:              "Hello, World!",
+		IsBase64Encoded:   false,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
